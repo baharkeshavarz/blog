0128d803974e75370ce62bcb2cca0f93a1f7c6c1
BaharKeshavarz <bahar.keshavarzc@gmail.com>
2023-10-31 18:15:10 +0300

    add main components with basic style

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import Sidebar from "./components/Sidebar";
+import Rightbar from "./components/Rightbar";
+import Feed from "./components/Feed";
+import { Box, Stack } from "@mui/material";
+
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    return (
+      <Box>
+        <Stack 
+               direction="row"
+               spacing={2}
+               justifyContent="space-between"
+            >
+            <Sidebar/>
+            <Feed/>
+            <Rightbar/>
+        </Stack>
+      </Box>
+    );
 }
 
 export default App;
