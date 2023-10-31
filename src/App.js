365c8834b31d9137539c7f607d0bcb3c21e40ff0
BaharKeshavarz <bahar.keshavarzc@gmail.com>
2023-10-31 19:08:13 +0300

    add navber

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Sidebar from "./components/Sidebar";
 import Rightbar from "./components/Rightbar";
 import Feed from "./components/Feed";
+import Navbar from "./components/Navbar";
 import { Box, Stack } from "@mui/material";
 
 function App() {
     return (
       <Box>
+        <Navbar/>
         <Stack 
                direction="row"
                spacing={2}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,82 @@
-import { Box } from '@mui/material'
+import styled from '@emotion/styled'
+import { AppBar, Avatar, Badge, InputBase, Toolbar, Typography } from '@mui/material'
 import React from 'react'
+import SpaIcon from '@mui/icons-material/Spa';
+import MailIcon from '@mui/icons-material/Mail';
+import { NotificationAdd } from '@mui/icons-material';
+
+const StyledToolbar = styled(Toolbar)({
+   display: "flex",
+   justifyContent: "space-between"
+})
+
+const Search = styled("div")(({ theme }) => ({
+  backgroundColor: "white",
+  padding: "0 10px",
+  borderRadius: theme.shape.borderRadius,
+  width: "40%",
+}));
+
+const Icons = styled("Box")(({ theme }) => ({
+   display: "none",
+   justifyContent: "center",
+   alignItems: "center",
+   gap: "15px",
+   [theme.breakpoints.up("sm")]: {
+      display: "flex"
+   }
+}))
+
+const UserBox = styled("Box")(({ theme }) => ({
+  display: "none",
+  justifyContent: "center",
+  alignItems: "center",
+  gap: "10px",
+  [theme.breakpoints.down("sm")]: {
+    display: "flex"
+  }
+}))
 
 const Navbar = () => {
   return (
-    <Box>Navbar</Box>
+    <AppBar position="sticky">
+        <StyledToolbar>
+             <Typography 
+                 variant="h6"
+                 sx={{ display: { xs: "none", sm: "block" }}}
+                 >
+                  Green Spa
+             </Typography>
+             <SpaIcon
+                sx={{ display: { xs: "block", sm: "none" }}}
+             />
+             <Search>
+                <InputBase placeholder="Search..."/>
+             </Search>
+             <Icons>
+                  <Badge badgeContent={4} color="error">
+                      <MailIcon color="action"/>
+                  </Badge>
+                  <Badge badgeContent={3} color="error">
+                       <NotificationAdd color="action"/>
+                  </Badge>
+                  <Avatar
+                       alt="Remy Sharp"
+                       src="/images/avatar/1.jpg"
+                       sx={{ width: 30, height: 30 }}
+                   />
+             </Icons>
+
+             <UserBox>
+                   <Avatar
+                       alt="Remy Sharp"
+                       src="/images/avatar/1.jpg"
+                       sx={{ width: 30, height: 30 }}
+                   />
+                   <Typography variant="span">John</Typography>
+             </UserBox>
+        </StyledToolbar>
+    </AppBar>
   )
 }
 
