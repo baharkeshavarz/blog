04ff677984666cdecf8bbcee80f4c56fbaf8d6fa
BaharKeshavarz <bahar.keshavarzc@gmail.com>
2023-11-01 08:57:34 +0300

    complete the sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,93 @@
-import { Box } from '@mui/material'
-import React from 'react'
+import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Switch } from '@mui/material'
+import HomeIcon from '@mui/icons-material/Home';
+import ArticleIcon from '@mui/icons-material/Article';
+import Diversity2Icon from '@mui/icons-material/Diversity2';
+import StorefrontIcon from '@mui/icons-material/Storefront';
+import ProfileIcon from '@mui/icons-material/Person4';
+import SettingsIcon from '@mui/icons-material/Settings';
+import FriendsIcon from '@mui/icons-material/Diversity3';
+import ModeNightIcon from '@mui/icons-material/ModeNight';
 
 const Sidebar = () => {
   return (
     <Box 
       flex={1}
       sx={{ display: { xs: "none", sm: "block"}}}
-      >Sidebar</Box>
+      >
+        <List>
+           <ListItem>
+              <ListItemButton component="a" href="#page">
+                  <ListItemIcon>
+                      <HomeIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Home Page" />
+              </ListItemButton>
+           </ListItem>
+
+           <ListItem>
+               <ListItemButton component="a" href="#pages">
+                  <ListItemIcon>
+                     <ArticleIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Pages"/>
+               </ListItemButton>
+           </ListItem>
+
+           <ListItem>
+               <ListItemButton component="a" href="#groups">
+                  <ListItemIcon>
+                     <Diversity2Icon />
+                  </ListItemIcon>
+                  <ListItemText primary="Groups"/>
+               </ListItemButton>
+           </ListItem>
+
+           <ListItem>
+               <ListItemButton component="a" href="#marketplace">
+                  <ListItemIcon>
+                     <StorefrontIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Marketplace"/>
+               </ListItemButton>
+           </ListItem>
+
+           <ListItem>
+               <ListItemButton component="a" href="#friends">
+                  <ListItemIcon>
+                     <FriendsIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Freinds"/>
+               </ListItemButton>
+           </ListItem>
+
+           <ListItem>
+               <ListItemButton component="a" href="#setting">
+                  <ListItemIcon>
+                     <SettingsIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Setting"/>
+               </ListItemButton>
+           </ListItem>
+
+           <ListItem>
+               <ListItemButton component="a" href="#profile">
+                  <ListItemIcon>
+                     <ProfileIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Profile"/>
+               </ListItemButton>
+           </ListItem>
+
+           <ListItem>
+               <ListItemButton component="a" href="#moon-night">
+                  <ListItemIcon>
+                     <ModeNightIcon />
+                  </ListItemIcon>
+                  <Switch />
+               </ListItemButton>
+           </ListItem>
+        </List>
+      </Box>
   )
 }
 
